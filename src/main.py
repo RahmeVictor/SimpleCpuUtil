83069e2c6f3985ad3399d73f70e2c0d295e7c0ee
Rahme Victor <42947472+NonuserOrg@users.noreply.github.com>
2020-07-27 21:56:24 +0300

    Cleanup and organized code

--- src/main.py
+++ src/main.py
@@ -10,26 +10,36 @@ if euid != 0:
     os.execlpe('sudo', *args)  # Does not work in pycharm console
 
 
-def main():
-    coreFile1 = "/sys/devices/system/cpu/cpu"
-    coreFile2 = "/cpufreq/scaling_governor"
-    cpuCount = os.cpu_count()
-    print(cpuCount)
+CORE_COUNT = os.cpu_count()
+# Each core file is located in /sys/devices/system/cpu/cpu"X"/cpufreq/scaling_governor, where X is the core number
+CORE_FILE1 = "/sys/devices/system/cpu/cpu"
+CORE_FILE2 = "/cpufreq/scaling_governor"
+
+
+def get_governor():
+    cpuGovs = []
     # Get current governor for all cores
-    for i in range(cpuCount):
+    for i in range(CORE_COUNT):
         coreNumber = str(i)
-        coreFile = "".join((coreFile1, coreNumber, coreFile2))
+        coreFile = "".join((CORE_FILE1, coreNumber, CORE_FILE2))
         fileGov = open(coreFile, "r")
         getGov = fileGov.read()
-        print(i, ": ", getGov)
+        cpuGovs.append(getGov)
 
-    profile = int(input("1-set to performance \n2-set to ondemand \n3-set to powersave\n0-exit \n"))
-    if profile == 0:
-        return 0
+    # See if any of the cores have a different governor
+    mainGov = cpuGovs[1]
+    for i in range(1, CORE_COUNT):
+        if cpuGovs[i] != mainGov:
+            mainGov = "different"
 
-    for i in range(cpuCount):
+    return mainGov
+
+
+def set_governor(profile):
+    # Find the file that contains the governor for each cpu core and write to it the requested governor
+    for i in range(CORE_COUNT):
         coreNumber = str(i)
-        coreFile = "".join((coreFile1, coreNumber, coreFile2))
+        coreFile = "".join((CORE_FILE1, coreNumber, CORE_FILE2))
         with open(coreFile, "w") as file:
             if profile == 1:
                 file.write("performance")
@@ -41,4 +51,20 @@ def main():
                 file.write("powersave")
 
 
-main()
+def display():
+    mainGov = get_governor()
+    print("Current governor: ", mainGov)
+
+
+def main():
+    profile = int(input())
+    if profile == 0:
+        return 1
+    set_governor(profile)
+    display()
+
+
+print("1-set to performance \n2-set to ondemand \n3-set to powersave\n0-exit \n")
+status = 0
+while status != 1:
+    status = main()
