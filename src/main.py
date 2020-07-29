2b8e8b10c4b9d5144060e467c20df997be9b8683
Rahme Victor <42947472+NonuserOrg@users.noreply.github.com>
2020-07-29 11:33:56 +0300

    Initial step to running profiles based on running programs

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
+import psutil
+
+
 # Check for root permissions
 euid = os.geteuid()
 if euid != 0:
@@ -14,6 +17,36 @@ CORE_COUNT = os.cpu_count()
 # Each core file is located in /sys/devices/system/cpu/cpu"X"/cpufreq/scaling_governor, where X is the core number
 CORE_FILE1 = "/sys/devices/system/cpu/cpu"
 CORE_FILE2 = "/cpufreq/scaling_governor"
+CONFIG_FILE = "/home/" + os.getenv('SUDO_USER') + "/.config/SimpleCpuUtil/config.ini"
+
+
+def get_settings():
+    """
+    Gets and applies settings.
+    """
+    watchedPrograms = {}
+    with open(CONFIG_FILE, "r") as config:
+        setting = config.readline().strip()  # Read each line and strip it of \n and trailing white space
+        programSetting = setting.partition(" = ")
+        watchedPrograms[programSetting[0]] = programSetting[2]  # Put the program in a dictionary {program : profile}
+        print(watchedPrograms)
+
+    return watchedPrograms
+
+
+def check_process_status(processName):
+    """
+    Check if there is any running process that contains the given name processName.
+    """
+    # Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False
 
 
 def get_governor():
@@ -41,14 +74,18 @@ def set_governor(profile):
         coreNumber = str(i)
         coreFile = "".join((CORE_FILE1, coreNumber, CORE_FILE2))
         with open(coreFile, "w") as file:
-            if profile == 1:
+            if profile == "performance":
                 file.write("performance")
 
-            elif profile == 2:
+            elif profile == "ondemand":
                 file.write("ondemand")
 
-            elif profile == 3:
+            elif profile == "powersave":
                 file.write("powersave")
+
+            else:
+                print("Profile not found")
+                return
 
 
 def display():
@@ -57,14 +94,19 @@ def display():
 
 
 def main():
-    profile = int(input())
-    if profile == 0:
-        return 1
-    set_governor(profile)
-    display()
+    print("Type what profile do you want to set or 0 to exit.")
+    # Set a profile based on running programs.
+    watchedPrograms = get_settings()
+    for program in watchedPrograms:
+        if check_process_status(program):
+            pass
+
+    while True:
+        display()
+        profile = input()
+        if profile == "0":
+            return
+        set_governor(profile)
 
 
-print("1-set to performance \n2-set to ondemand \n3-set to powersave\n0-exit \n")
-status = 0
-while status != 1:
-    status = main()
+main()
